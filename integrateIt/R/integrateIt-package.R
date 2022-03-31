80a814edd62e88a97115c04c01d7679f5b4b69c6
Kimberly Acquilano <97560083+kacquilano1@users.noreply.github.com>
2022-03-31 17:56:09 -0500

    Updated Trapezoid
    On line 17, added integrateIt-generic
    and on line 42 deleted the duplicate @rdname which was throwing warnings and notes.

--- integrateIt/R/integrateIt-package.R
+++ integrateIt/R/integrateIt-package.R
@@ -3,6 +3,7 @@
 #' The integrateIt package performs approximations of integrats.
 #' @name integrateIt
 #' @docType package
+#' @aliases integrateIt-package
 #' @author  Kimberly Acquilano: \email{k.a.acquilano@@wustl.edu} and
 #' @examples
 #'
