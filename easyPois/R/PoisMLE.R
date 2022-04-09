127821a0f3956d7690503968367eeb6140f94c06
Kimberly Acquilano <97560083+kacquilano1@users.noreply.github.com>
2022-04-09 13:07:14 -0500

    Created easyPois package
    Created easyPois package
    Created PoisMLE class.
    Editied Documentation page

---
+++ easyPois/R/PoisMLE.R
@@ -0,0 +1,49 @@
+#' An object using the Poisson Distribution
+#'
+#' Objects of class \code{PoisMLE} are created by the \code{estimatePois} function
+#'
+#'
+#' An object of the class `PoisMLE' has the following slots:
+#' \itemize{
+#' \item \code{y} A vector of data
+#' \item \code{MLE} The maximum likelihood estimator
+#' \item \code{LL} The log likelihood calculated from the vector of data
+#' \item \code{SE} The standard error for the MSE
+#' \item \code{SEtype} The method used to calculate the standard error
+#' }
+#'
+#' @author Kimberly Acquilano \email{k.a.acquilano@@wustl.edu}
+#' @import methods
+#' @aliases PoisMLE-class initialize,estimatePois-method,SEtype-method
+#' @rdname PoisMLE
+#' @export
+setClass(Class="PoisMLE",
+         representation = representation(
+           y = "numeric",
+           MLE = "numeric",
+           LL = "numeric",
+           SE = "numeric",
+           SEtype = "character"
+         ),
+         prototype = prototype(
+           y = 0,
+           MLE = 0,
+           LL = 0,
+           SE = 0,
+           SEtype = ""
+         )
+)
+
+#' @export
+setMethod("initialize", "PoisMLE",
+          function(.Object, ...){
+            value=callNextMethod()
+            return(value)
+          })
+
+#' @export
+setMethod(f = "print",
+          signature(x = "PoisMLE"),
+          definition = function(x) {
+            print(x@MLE, x@LL, x@SE, x@SEtype)
+          })
